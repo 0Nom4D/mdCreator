d18e8286f926fa4b9a37753ea818b3f1b1dcc7cd
Nom4D- <arthur.adam@epitech.eu>
2021-05-12 22:10:32 +0200

    mdCreator - Updating Prerequisites Download Links

--- src/prerequisites.py
+++ src/prerequisites.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 def cPlusPlusPrerequisites(fileDesc):
-    fileDesc.write("To use this project, you'll need C++.\n\n")
+    fileDesc.write("To use this project, you'll need G++ Compiler.\n\n")
     return (0)
 
 def pythonPrerequisites(fileDesc):
-    fileDesc.write("To use this project, you'll need python.\n")
+    fileDesc.write("To use this project, you'll need Python (Version 3.8):\n\n* [Python Installation](https://www.python.org/downloads/)\n")
     return (0)
 
 def cPrerequisites(fileDesc):
-    fileDesc.write("To use this project, you'll need C.\n\n")
+    fileDesc.write("To use this project, you'll need GCC Compiler.\n\n")
     return (0)
 
 def haskellPrerequisites(fileDesc):
-    fileDesc.write("To use this project, you'll need haskell.\n\n")
+    fileDesc.write("To use this project, you'll need Stack:\n\n* [Stack Installation Guide](https://docs.haskellstack.org/en/stable/install_and_upgrade/)\n")
     return (0)
 
 def noPrerequisites(fileDesc):
