ca9fa4a68ee72faf47726de06d81a6a772b2d1fd
Arthur Adam <60994056+0Nom4D@users.noreply.github.com>
2021-06-25 02:00:18 +0200

    mdCreator - Adding New version of files

---
+++ src/Prerequisites.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def cPlusPlusPrerequisites(fileDesc):
+    fileDesc.write("To use this project, you'll need G++ Compiler.\n\n")
+    return (0)
+
+def pythonPrerequisites(fileDesc):
+    fileDesc.write("To use this project, you'll need Python (Version 3.8):\n\n* [Python Installation](https://www.python.org/downloads/)\n\n")
+    return (0)
+
+def cPrerequisites(fileDesc):
+    fileDesc.write("To use this project, you'll need GCC Compiler.\n\n")
+    return (0)
+
+def haskellPrerequisites(fileDesc):
+    fileDesc.write("To use this project, you'll need Stack:\n\n* [Stack Installation Guide](https://docs.haskellstack.org/en/stable/install_and_upgrade/)\n\n")
+    return (0)
+
+def noPrerequisites(fileDesc):
+    fileDesc.write("mdCreator didn't find your language prerequisites. Please edit this part.\n\n")
+    return (0)
